169728e8a498cb07a90d2dbf365758b954e4dce1
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2025-09-28 01:39:22 +0200

    Adds CSS linter

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("stylelint").Config} */
+export default {
+  "extends": ["stylelint-config-standard"],
+  "rules": {
+    "media-feature-range-notation": "prefix",
+    "declaration-block-no-redundant-longhand-properties": null,
+    "shorthand-property-no-redundant-values": null,
+    "property-no-vendor-prefix": null,
+  }
+};
